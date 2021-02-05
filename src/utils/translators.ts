be68c9e0177acfdea99e10843a767a52b85ba395
Alexander Teusz <teusz.alexander@gmail.com>
2021-02-05 16:50:32 +0100

    removed unused import

--- src/utils/translators.ts
+++ src/utils/translators.ts
@@ -1,5 +1,4 @@
 import chalk = require("chalk");
-import { ExecFileOptionsWithStringEncoding } from "child_process";
 
 // Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate').v2;
