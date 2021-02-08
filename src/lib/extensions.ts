a8a6416de5a1950636b6dd72e3fe33e6ee2f4bff
Alexander Teusz <teusz.alexander@gmail.com>
2021-02-08 17:34:43 +0100

    initially added extensions file in lib

---
+++ src/lib/extensions.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import chalk = require('chalk');
+
+import CognigyClient from '../utils/cognigyClient';
+import CONFIG from '../utils/config';
+import { indexAll } from '../utils/indexAll';
+
+import { IExtensionIndexItem_2_0 } from '@cognigy/rest-api-client';
+
+/**
+ * Updates Extensions definitons from server (every x seconds)
+ * @param cacheTime Seconds to cache locales
+ */ 
+export const pullExtensions = async (cacheTime: number = 10): Promise<IExtensionIndexItem_2_0[]> => {
+    const extensionsLocation = CONFIG.agentDir + "/extensions/extension.json";
+
+    let extensionsAge = cacheTime + 1;
+    let extensions = null;
+
+    /* Check if Extensions folder exists */
+    if (fs.existsSync(extensionsLocation)) {
+        const stats = fs.statSync(extensionsLocation);
+        const mtime = stats.mtime;
+        extensionsAge = (new Date().getTime() - mtime.getTime()) / 1000;
+    }
+
+    /* If Extensions are stale, update them from server */
+    if (extensionsAge > cacheTime) {
+        const extensionsResult = await indexAll(CognigyClient.indexExtensions)({
+            projectId: CONFIG.agent
+        });
+        extensions = extensionsResult.items;
+
+        fs.writeFileSync(extensionsLocation, JSON.stringify(extensionsResult.items, undefined, 4));
+    } else {
+        extensions = JSON.parse(fs.readFileSync(extensionsLocation).toString());
+    }
+
+    return extensions;
+};
