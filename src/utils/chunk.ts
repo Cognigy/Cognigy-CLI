43d6b9edca6875b1b403756a3ec0f3be5ed9e481
Fabio Colajanni <f.colajanni@cognigy.com>
2024-02-26 11:57:41 +0100

    fix(bug): clean-up code on utils chunk

--- src/utils/chunk.ts
+++ src/utils/chunk.ts
@@ -1,8 +1,8 @@
 export const chunkArray = <T>(arr: T[], size: number): T[][] => {
-    var chunkedArr = [];
+    let chunkedArr = [];
     arr = arr || [];
     size = size === undefined ? 1 : size;
-    for (var i = 0; i < arr.length; i += size) {
+    for (let i = 0; i < arr.length; i += size) {
         chunkedArr.push(arr.slice(i, i + size));
     }
     return chunkedArr;
