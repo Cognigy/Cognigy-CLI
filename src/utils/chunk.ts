31c770f9798c46d73c7d303714d507bc22eb6212
Fabio Colajanni <f.colajanni@cognigy.com>
2024-02-26 14:16:00 +0100

    feat(chunk): improve chunk util function

--- src/utils/chunk.ts
+++ src/utils/chunk.ts
@@ -1,9 +1,8 @@
-export const chunkArray = <T>(arr: T[], size: number): T[][] => {
-    let chunkedArr = [];
-    arr = arr || [];
-    size = size === undefined ? 1 : size;
-    for (let i = 0; i < arr.length; i += size) {
-        chunkedArr.push(arr.slice(i, i + size));
-    }
-    return chunkedArr;
+export const chunkArray = <T>(inputArray: T[], size: number): T[][] => {
+    return inputArray.reduce((resultArray, _, i) => {
+        if (i % size === 0) {
+            resultArray.push(inputArray.slice(i, i + size));
+        }
+        return resultArray;
+    }, []);
 };
